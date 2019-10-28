2f664c666085a43a18bc0c3c25401729acb96a92
Sammy <lixiangmin01@baidu.com>
2019-10-28 19:32:11 +0800

    feature(ffi/called-by-go): demo exporting rust struct

--- ffi/called-by-go/go/main.go
+++ ffi/called-by-go/go/main.go
@@ -2,7 +2,11 @@ package main
 
 //#cgo LDFLAGS: -L${SRCDIR}/../build/target/debug -lhelloworld
 //#include <stdlib.h>
+//typedef struct HelloWorld hello_world_t;
 //extern void hello_world(const char* who);
+//extern hello_world_t *hello_world_new(const char *who);
+//extern void hello_world_free(hello_world_t *);
+//extern void hello_world_say(const hello_world_t *);
 import "C"
 import "unsafe"
 
@@ -11,4 +15,9 @@ func main() {
 	defer C.free(unsafe.Pointer(who))
 
 	C.hello_world(who)
+
+	hw := C.hello_world_new(who)
+	defer C.hello_world_free(hw)
+
+	C.hello_world_say(hw)
 }
--- ffi/called-by-go/src/lib.rs
+++ ffi/called-by-go/src/lib.rs
@@ -1,11 +1,47 @@
-
-
 use std::ffi::CStr;
 use std::os::raw::c_char;
+
+pub struct HelloWorld {
+    who: String,
+}
+
+impl HelloWorld {
+    fn new(who: String) -> Self {
+        Self { who }
+    }
+}
 
 #[no_mangle]
 pub extern "C" fn hello_world(c_who: *const c_char) {
     let who = unsafe { CStr::from_ptr(c_who).to_str() };
 
     println!("hello {}, welcome to rust's world", who.unwrap());
+}
+
+#[no_mangle]
+pub extern "C" fn hello_world_free(ptr: *mut HelloWorld) {
+    if ptr.is_null() {
+        return;
+    }
+
+    unsafe {
+        Box::from_raw(ptr);
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn hello_world_new(c_who: *const c_char) -> *mut HelloWorld {
+    let who = unsafe { CStr::from_ptr(c_who).to_str().unwrap() };
+
+    Box::into_raw(Box::new(HelloWorld::new(who.to_string())))
+}
+
+#[no_mangle]
+pub extern "C" fn hello_world_say(ptr: *const HelloWorld) {
+    let h = unsafe {
+        assert!(!ptr.is_null());
+        &*ptr
+    };
+
+    println!("hello world, {}", &h.who);
 }
