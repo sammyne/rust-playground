7317f00854bb5da5779c7ef22e95d9680609f882
Sammy <lixiangmin01@baidu.com>
2019-10-28 19:19:03 +0800

    feature(ffi/called-by-c): demo exporting rust struct

---
+++ ffi/called-by-c/c/main.c
@@ -0,0 +1,28 @@
+#include <stdint.h>
+#include <stdio.h>
+
+// HelloWorld will be mapped to that of rust's HelloWorld automatically.
+// HelloWorld can be any valid name you want.
+typedef struct HelloWorld hello_world_t;
+
+extern hello_world_t *hello_world_new(const char *who);
+extern void hello_world_free(hello_world_t *);
+extern void hello_world_say(const hello_world_t *);
+
+extern int32_t double_input(int32_t input);
+
+int main()
+{
+    int input = 4;
+    int output = double_input(input);
+
+    printf("%d * 2 = %d\n", input, output);
+
+    hello_world_t *hw = hello_world_new("sammy");
+
+    hello_world_say(hw);
+
+    hello_world_free(hw);
+
+    return 0;
+}
--- ffi/called-by-c/src/lib.rs
+++ ffi/called-by-c/src/lib.rs
@@ -1,5 +1,45 @@
+use std::ffi::CStr;
+use std::os::raw::c_char;
+
+pub struct HelloWorld {
+    who: String,
+}
+
+impl HelloWorld {
+    fn new(who: String) -> Self {
+        Self { who }
+    }
+}
 
 #[no_mangle]
-pub extern fn double_input(input: i32) -> i32 {
+pub extern "C" fn double_input(input: i32) -> i32 {
     input * 2
+}
+
+#[no_mangle]
+pub extern "C" fn hello_world_free(ptr: *mut HelloWorld) {
+    if ptr.is_null() {
+        return;
+    }
+
+    unsafe {
+        Box::from_raw(ptr);
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn hello_world_new(c_who: *const c_char) -> *mut HelloWorld {
+    let who = unsafe { CStr::from_ptr(c_who).to_str().unwrap() };
+
+    Box::into_raw(Box::new(HelloWorld::new(who.to_string())))
+}
+
+#[no_mangle]
+pub extern "C" fn hello_world_say(ptr: *const HelloWorld) {
+    let h = unsafe {
+        assert!(!ptr.is_null());
+        &*ptr
+    };
+
+    println!("hello world, {}", &h.who);
 }
